1739513b36b2a901894435e1403d8a26ebefe7b2
Liancohen0104 <liancohen0104@gmail.com>
2026-02-12 21:03:49 +0200

    add doctor model

---
+++ server/Models/Doctor.cs
@@ -0,0 +1,42 @@
+using System.Text.Json.Serialization;
+
+namespace server.Models;
+
+public class Doctor
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("fullName")]
+    public string FullName { get; set; } = string.Empty;
+
+    [JsonPropertyName("phones")]
+    public List<Phone> Phones { get; set; } = [];
+
+    [JsonPropertyName("reviews")]
+    public DoctorReviews Reviews { get; set; } = new();
+
+    [JsonPropertyName("languageIds")]
+    public List<string> LanguageIds { get; set; } = [];
+    
+    [JsonPropertyName("isActive")]
+    public bool IsActive { get; set; }
+
+    [JsonPropertyName("promotionLevel")]
+    public int PromotionLevel { get; set; }
+}
+
+public class DoctorReviews
+{
+    [JsonPropertyName("averageRating")]
+    public double AverageRating { get; set; }
+
+    [JsonPropertyName("totalRatings")]
+    public double TotalRatings { get; set; }
+}
+
+public class Phone
+{
+    [JsonPropertyName("number")]
+    public string PhoneNumber { get; set; } = string.Empty;
+}
